e015fb5fd574110331f2fd522d25c3a88219cc07
dgouveia <dgouveia@davidgouveia.net>
2016-09-21 15:22:41 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # python-rss-parser
-The simplest and most basic rss parser for python
+The simplest and most basic rss parser for python.
+
+## What does it do? ##
+The workflow is pretty straightforward:
+ - Load an RSS url
+ - Parse it with regular expressions
+ - Extract the Link, Title and date from each item
+ - Return an array containing the information
+
+## Improvements ##
+There are so many things that could be improved that you might just write something new from scratch if you want something that implements the RSS standard decently. 
---
+++ rssparser.py
@@ -0,0 +1,27 @@
+import requests
+import re
+
+class RSSParser(object):
+  
+  items_regex         = re.compile('<item>(.*?)<\/item>', re.S)
+  details_title_regex = re.compile('<title>(.*?)<\/title>', re.S)
+  details_link_regex  = re.compile('<link>(.*?)<\/link>', re.S)
+  details_date_regex  = re.compile('<pubDate>(.*?)<\/pubDate>', re.S)
+ 
+  def parse(self, url):
+    ''' Very basic RSS parser. Reads an RSS feed and extracts all the items ( Title, Link, pubDate ) '''
+    items = []
+    response = requests.get(url).text
+    tmpItems = self.items_regex.findall(response)
+    for i in tmpItems:
+      items.append({"title": self.details_title_regex.search(i).group(1), "link": self.details_link_regex.search(i).group(1), "date": self.details_date_regex.search(i).group(1)})
+    return items
+
+if __name__ == "__main__":
+  import sys
+  try:
+    if len(sys.argv) > 1:
+      print RSSParser().parse(sys.argv[1])
+  except Exception as e:
+    print "Error parsing the feed you provided: %s" % e.message
+    sys.exit(1)
